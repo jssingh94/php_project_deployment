5bdcdb1d8f40c217ccd4af714beb1d651f6210c4
jssingh94 <108279307+jssingh94@users.noreply.github.com>
2025-05-23 12:54:31 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('ec2-13-201-1-204.ap-south-1.compute.amazonaws.com','root','admin123','oesm')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('localhost','root','admin123','oesm')or die("Could not connect to mysql".mysqli_error($con));
 
 
 ?>
